73db6522aec2b79491337025c6c9e71ada3fcfb0
Mahdiar Shoraka <mahdiarshoraka@gmail.com>
2025-05-09 10:27:00 -0400

    Added AbilitySystemComponent and AttributeSet for base character

---
+++ Source/SoulsClone/Public/AbilitySystem/SoulsAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// Mahdiar Shoraka All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "SoulsAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOULSCLONE_API USoulsAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SoulsClone/Public/AbilitySystem/SoulsAttributeSet.h
@@ -0,0 +1,17 @@
+// Mahdiar Shoraka All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "SoulsAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOULSCLONE_API USoulsAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
